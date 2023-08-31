1d786696a55bfad99f6fcbedfba97a7cffed1037
HectorGarcia01 <hg2779948@gmail.com>
2023-08-31 01:39:02 -0600

    Creación de módulo para el perfil del admin, con sus componentes ver y actualizar perfil, además de la configuración de sus respectivas rutas

--- src/app/admin/admin-profile/admin-profile-routing.module.ts
+++ src/app/admin/admin-profile/admin-profile-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ViewProfileComponent } from './components/view-profile/view-profile.component';
+import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
 
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'profile/view',
+    component: ViewProfileComponent
+  },
+  {
+    path: 'profile/update',
+    component: UpdateProfileComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/admin/admin-profile/admin-profile.module.ts
+++ src/app/admin/admin-profile/admin-profile.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminProfileRoutingModule } from './admin-profile-routing.module';
-
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ViewProfileComponent } from './components/view-profile/view-profile.component';
+import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
+import { AdminSharedModule } from '../shared/admin-shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    ViewProfileComponent,
+    UpdateProfileComponent
+  ],
   imports: [
     CommonModule,
-    AdminProfileRoutingModule
+    AdminProfileRoutingModule,
+    AdminSharedModule
   ]
 })
 export class AdminProfileModule { }
