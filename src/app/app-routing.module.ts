f964a0e7f30e56428d42d65bf7e8a012efbd396b
HectorGarcia01 <hg2779948@gmail.com>
2023-08-31 01:41:52 -0600

    Configuración de las rutas padres en el app-routing, además se cambió el titulo del proyecto y el idioma en el index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // Validar cualquier otro path y redireccionar a inicio
+  { path: '', redirectTo: '', pathMatch: 'full' },
+
+  //Rutas para el login
+
+  //Rutas para
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin-home/admin-home.module').then((m) => m.AdminHomeModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/customer-management/customer-management.module').then((m) => m.CustomerManagementModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin-profile/admin-profile.module').then((m) => m.AdminProfileModule),
+  },
+  //Rutas para el cliente
+  {
+    path: '',
+    loadChildren: () =>
+      import('./user/user-home/user-home.module').then((m) => m.UserHomeModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./user/user-profile/user-profile.module').then((m) => m.UserProfileModule),
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
