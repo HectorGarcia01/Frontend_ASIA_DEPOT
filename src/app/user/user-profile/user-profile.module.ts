021a3037228fd3eab2d3358c75db3755fcaf7a78
HectorGarcia01 <hg2779948@gmail.com>
2023-09-14 14:44:41 -0600

    Configuración de las rutas de los componentes para el perfil

--- src/app/user/user-profile/user-profile.module.ts
+++ src/app/user/user-profile/user-profile.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { ViewProfileComponent } from './components/view-profile/view-profile.component';
-import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
+import { UpdateProfileComponent } from './pages/update-profile/update-profile.component';
 import { UserSharedModule } from '../shared/user-shared.module';
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    ViewProfileComponent,
     UpdateProfileComponent
   ],
   imports: [
