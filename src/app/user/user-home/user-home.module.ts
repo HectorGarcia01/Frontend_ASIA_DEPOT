4cde2e5160a1e7bbb09bc8cdfa28971f3329c68e
HectorGarcia01 <hg2779948@gmail.com>
2023-09-20 17:53:10 -0600

    Creación de los componentes en pages de about (Nosotros) y de contact-us, además de la configuración de sus respectivas rutas en el routing

--- src/app/user/user-home/user-home.module.ts
+++ src/app/user/user-home/user-home.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserHomeRoutingModule } from './user-home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { UserSharedModule } from '../shared/user-shared.module';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ContactUsComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
