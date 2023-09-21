3d0d13a3954a41fd72a5563af52f702eb2e6b506
HectorGarcia01 <hg2779948@gmail.com>
2023-09-21 03:39:08 -0600

    Creación de los componentes para la sección de categorías, para la sección de ofertas de productos, para la sección de productos relevantes o destacados y para la sección de reseñas

--- src/app/user/user-home/user-home.module.ts
+++ src/app/user/user-home/user-home.module.ts
@@ -6,12 +6,20 @@ import { HomeComponent } from './pages/home/home.component';
 import { UserSharedModule } from '../shared/user-shared.module';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CategoriesSectionComponent } from './components/categories-section/categories-section.component';
+import { ProductOffersSectionComponent } from './components/product-offers-section/product-offers-section.component';
+import { ProductsSectionComponent } from './components/products-section/products-section.component';
+import { ReviewsSectionComponent } from './components/reviews-section/reviews-section.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ContactUsComponent,
-    AboutComponent
+    AboutComponent,
+    CategoriesSectionComponent,
+    ProductOffersSectionComponent,
+    ProductsSectionComponent,
+    ReviewsSectionComponent
   ],
   imports: [
     CommonModule,
