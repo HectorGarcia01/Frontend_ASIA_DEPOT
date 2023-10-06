7a871c751d34532cf07ddf068b7349e93e9a52e8
HectorGarcia01 <hg2779948@gmail.com>
2023-10-06 14:02:44 -0600

    Creación de la función para validar si está autenticado por cookie y una para cerrar sesión la cual elimina la cookie de autenticación

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
+
+  /**
+   * Función para validar si está autenticado
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
+  isAuthenticated(): boolean {
+    return this.cookieService.check('authCookie');
+  }
+
+  /**
+   * Función para cerrar sesión (eliminar cookie)
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
+  logout(): void {
+    this.cookieService.delete('authCookie');
+  }
 }
