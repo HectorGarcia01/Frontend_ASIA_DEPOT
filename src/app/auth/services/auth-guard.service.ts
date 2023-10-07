e7eb2a4d3a28c6fc578179149abdcab8cf51ccdf
HectorGarcia01 <hg2779948@gmail.com>
2023-10-06 23:22:17 -0600

    Validación para los roles de admin y superAdmin, de lo contrario se denegará el acceso

--- src/app/auth/services/auth-guard.service.ts
+++ src/app/auth/services/auth-guard.service.ts
@@ -10,14 +10,21 @@ export class AuthGuardService implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   /**
-   * Función para validar la protección de rutas
+   * Función para validar la protección de rutas del admin/superAdmin
    * Fecha creación: 06/10/2023
    * Autor: Hector Armando García González
    */
 
   canActivate(): boolean {
     if (this.authService.isAuthenticated()) {
-      return true;
+      const userRole = this.authService.getRole();
+
+      if (userRole === 'admin' || userRole === 'superAdmin') {
+        return true;
+      } else {
+        //***************por el momento un alert xd */
+        return false;
+      }
     } else {
       this.router.navigate(['/login']);
       return false;
