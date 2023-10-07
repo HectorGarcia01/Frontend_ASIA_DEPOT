58a70df95c709b92fec3cc17ad5f5d23fabea277
HectorGarcia01 <hg2779948@gmail.com>
2023-10-06 23:25:29 -0600

    Creación del servicio para proteger las rutas del user, verifica si está autenticado y que si la persona posee el rol de user, de lo contrario se deniega el acceso

--- src/app/auth/services/auth-user-guard.service.ts
+++ src/app/auth/services/auth-user-guard.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthUserGuardService {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  /**
+   * Función para validar la protección de rutas del user
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
+  canActivate(): boolean {
+    if (this.authService.isAuthenticated()) {
+      const userRole = this.authService.getRole();
+
+      if (userRole === 'user') {
+        return true;
+      } else {
+        //***************por el momento un alert xd */
+        return false;
+      }
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
 }
