d3b19b903b12e00f126138a2cbe77218dfe9faa0
HectorGarcia01 <hg2779948@gmail.com>
2023-10-09 00:45:37 -0600

    Consumiendo el servicio para obtener todas las categías existentes y utilizando un ngFor para crear las cards de cada categoría

--- src/app/user/user-home/components/categories-section/categories-section.component.ts
+++ src/app/user/user-home/components/categories-section/categories-section.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/user/services/category.service';
+import { Category, Categories } from 'src/app/user/interfaces/category.interface';
 
 @Component({
   selector: 'app-categories-section',
   templateUrl: './categories-section.component.html',
   styleUrls: ['./categories-section.component.css']
 })
-export class CategoriesSectionComponent {
+export class CategoriesSectionComponent implements OnInit {
+  category: Category[] = [];
+  categories: Categories[] = [];
+  
+  constructor(
+    private categoryService: CategoryService
+  ) { }
 
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories('http://localhost:3000/usuario/ver/categorias').subscribe((data: any) => {
+      this.category = data.categories;
+    });
+  }
 }
