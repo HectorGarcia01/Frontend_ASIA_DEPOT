d19792b47af2c4ac24cb1b90fe34a5851f177f7d
HectorGarcia01 <hg2779948@gmail.com>
2023-10-09 12:48:26 -0600

    Consumo del servicio para listar todas las categorías en el footer

--- src/app/user/shared/components/footer/footer.component.ts
+++ src/app/user/shared/components/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/user/services/category.service';
+import { Category, Categories } from 'src/app/user/interfaces/category.interface';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  category: Category[] = [];
+  categories: Categories[] = [];
 
+  constructor(
+    private categoryService: CategoryService
+  ) { }
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories('http://localhost:3000/usuario/ver/categorias').subscribe((data: any) => {
+      this.category = data.categories;
+    });
+  }
 }
