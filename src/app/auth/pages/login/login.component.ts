397fb2454ffb926630d612dd930f60e08a9f0010
HectorGarcia01 <hg2779948@gmail.com>
2023-10-10 03:40:03 -0600

    Consumo del servicio para el inicio de sesión

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service'; 
+import { SingIn } from '../../interfaces/login.interface';
+import { getCustomer } from 'src/app/user/interfaces/customer.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  customerData: getCustomer[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.validateForm();
+  }
+
+  ngOnInit() {
+    
+  }
+
+  sweetAlertPersonalizada(icono:any, titulo:any, msgError:any) {
+    Swal.fire({
+      icon: icono,
+      title: titulo,
+      text: msgError,
+      showConfirmButton: false,
+      timer: 3000
+    });
+  }
+
+  private validateForm() {
+    this.loginForm = new FormGroup({
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  singIn() {
+    if (this.loginForm.valid) {
+      const signIn: SingIn = {
+        correo: this.loginForm.get('correo')?.value,
+        password: this.loginForm.get('password')?.value
+      }
+
+      this.authService.singIn('http://localhost:3000/usuario/login', signIn).subscribe((data: any) => {
+        if (data.success) {
+          this.sweetAlertPersonalizada('success', "Exitoso", `Bienvenido ${data.user.Nombre_Cliente}`);
+          this.customerData = data.user;
+          console.log(this.customerData);
+          this.router.navigate(['/home']);
+        } else {
+          console.log(data.error);
+          this.sweetAlertPersonalizada('error', "Error", data.error);
+        }
+      });
+    }
+  }
 }
