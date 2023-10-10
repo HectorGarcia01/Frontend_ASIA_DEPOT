dacac72bb69457aac7828a065fad00cf32c99a9c
HectorGarcia01 <hg2779948@gmail.com>
2023-10-10 11:31:26 -0600

    Modificación del servicio para iniciar sesión, se agregó lo de la obtención del rol del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -18,7 +18,9 @@ export class AuthService {
 
   singIn(url: string, user: any): Observable<any> {
     return this.http.post<any>(url, user).pipe(
-      
+      tap((data: any) => {
+        this.roleSubject.next(data.userRole);
+      })
     );
   }
 
