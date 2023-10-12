8ea4bb70531d2752c3e2bc8fd4694525fe8aef70
HectorGarcia01 <hg2779948@gmail.com>
2023-10-12 16:09:17 -0600

    Agregando comentarios de referencia a sus respectivos servicios

--- src/app/user/services/category.service.ts
+++ src/app/user/services/category.service.ts
@@ -8,6 +8,12 @@ export class CategoryService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Función para realizar una solicitud get para listar categorías
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
   getCategories(url: string) {
     return this.http.get(url);
   }
