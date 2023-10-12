981c0e2ef95ccad4d2905835ea38acdd91e326e0
HectorGarcia01 <hg2779948@gmail.com>
2023-10-12 15:38:40 -0600

    Se agregó un router navigate hacia a home cuando se cierre la sesión del cliente

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,15 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private router: Router
   ) { }
+
+  /**
+   * Función para realizar una solicitud post de inicio de sesión
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
 
   singIn(url: string, user: any): Observable<any> {
     return this.http.post<any>(url, user).pipe(
@@ -23,6 +31,12 @@ export class AuthService {
       })
     );
   }
+
+  /**
+   * Función para guardar la cookie
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
 
   saveCookieAuth() {
     const token = this.cookieService.get('authCookie');
@@ -57,5 +71,6 @@ export class AuthService {
 
   logout(): void {
     this.cookieService.delete('authCookie');
+    this.router.navigate(['/home']);
   }
 }
