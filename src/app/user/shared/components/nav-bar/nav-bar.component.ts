0f79cb383554191f3406aa93f2d95f2253dafcbf
HectorGarcia01 <hg2779948@gmail.com>
2023-10-12 15:43:42 -0600

    Creación de la función la cual valida si el cliente está autenticado para mostrarle ciertos botones o no, además de la función para cerrar la sesión lo que borra la cookie del navegador

--- src/app/user/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/user/shared/components/nav-bar/nav-bar.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  isAuthenticated: boolean = false;
 
   constructor(
     public authService: AuthService
-    ) { }
+  ) { }
 
   ngOnInit() {
     
+  }
+
+  /**
+   * Función para verificar si el cliente está autenticado
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  /**
+   * Función para cerrar la sesión del cliente
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+
+  logout(): void {
+    this.authService.logout();
   }
 }
