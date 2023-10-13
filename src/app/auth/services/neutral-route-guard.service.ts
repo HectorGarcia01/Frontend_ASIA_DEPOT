ab0c41099df3dbcf4e13f82abf8430ab5517167d
HectorGarcia01 <hg2779948@gmail.com>
2023-10-12 18:41:00 -0600

    Creación del servicio para proteger las rutas neutrales del acceso del administrador, quiere decir que media vez sea un administrado no tendrá acceso, en caso de ser un User o no estar autenticado si se le dará acceso

---
+++ src/app/auth/services/neutral-route-guard.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { CustomAlertService } from 'src/app/services/custom-alert.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NeutralRouteGuardService {
+
+  constructor(
+    private authService: AuthService,
+    private customAlertService: CustomAlertService,
+    private router: Router
+  ) { }
+
+  /**
+   * Función para validar la protección de rutas neutras
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *            Función isAuthenticated del servicio de autenticación (auth.service),
+   *            Función getRole del servicio de autenticación (auth.service),
+   *            Función sweetAlertPersonalizada del servicio de alerta personalizada (custom-alert.service)
+   */
+
+  canActivate(): boolean {
+    if (this.authService.isAuthenticated()) {
+      const userRole = this.authService.getRole();
+
+      if (userRole === 'Admin' || userRole === 'SuperAdmin') {
+        this.customAlertService.sweetAlertPersonalizada('error', "Permisos denegados", "No posees los permisos necesarios");
+        this.router.navigate(['/admin']);
+        return false;
+      }
+    }
+    return true;
+  }
+}
