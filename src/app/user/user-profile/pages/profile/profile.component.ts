361b34766b2a7b92158d2a5e863458770916886a
HectorGarcia01 <hg2779948@gmail.com>
2023-10-30 01:08:48 -0600

    Versión beta de ver perfil

--- src/app/user/user-profile/pages/profile/profile.component.ts
+++ src/app/user/user-profile/pages/profile/profile.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserServicesService } from 'src/app/user/services/user-services.service';
+import { apiURL } from 'src/app/config/config';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  selectedFile: File | undefined;
+  uploading: boolean = false;
 
+  constructor(private userService: UserServicesService) { }
+
+  ngOnInit(){
+    
+  }
+
+  onUploadAvatar() {
+    if (this.selectedFile) {
+      // const formData = new FormData();
+      // formData.append('avatar', this.selectedFile);
+
+      this.uploading = true;
+      this.userService.uploadProfilePhoto(`${apiURL}/usuario/subir/avatar`, this.selectedFile).subscribe(
+        (response) => {
+          alert("todo bien")
+          this.uploading = false;
+        },
+        (error) => {
+          alert(error.message)
+          this.uploading = false;
+        }
+      );
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      this.selectedFile = inputElement.files[0];
+    }
+  }
 }
