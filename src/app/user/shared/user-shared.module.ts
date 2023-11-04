8e92c7dd68c728649fef58b3a73fbdb906a7bde2
HectorGarcia01 <hg2779948@gmail.com>
2023-11-04 02:35:09 -0600

    Importación del modulo para el formulario reactivo

--- src/app/user/shared/user-shared.module.ts
+++ src/app/user/shared/user-shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -12,6 +14,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule
   ],
   exports: [
