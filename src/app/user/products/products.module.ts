19ff29bba944076dd9d07e5283ce9df636545b4b
HectorGarcia01 <hg2779948@gmail.com>
2023-11-07 03:30:41 -0600

    Eliminación del componente para información adicional

--- src/app/user/products/products.module.ts
+++ src/app/user/products/products.module.ts
@@ -5,7 +5,6 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { UserSharedModule } from '../shared/user-shared.module';
-import { ProductDescriptionComponent } from './components/product-description/product-description.component';
 import { ProductReviewsComponent } from './components/product-reviews/product-reviews.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { ProductReviewsComponent } from './components/product-reviews/product-re
   
     ProductListComponent,
     ProductDetailComponent,
-    ProductDescriptionComponent,
     ProductReviewsComponent
   ],
   imports: [
