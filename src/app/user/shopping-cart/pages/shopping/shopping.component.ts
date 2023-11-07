030d2825d48db7b9c44e01ac201972d5c915a593
HectorGarcia01 <hg2779948@gmail.com>
2023-11-06 18:08:15 -0600

    Creación de la estructura del carrito de compras

--- src/app/user/shopping-cart/pages/shopping/shopping.component.ts
+++ src/app/user/shopping-cart/pages/shopping/shopping.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/user/services/shopping-cart.service';
+import { CustomAlertService } from 'src/app/services/custom-alert.service';
+import { apiURL } from 'src/app/config/config';
 
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.css']
 })
-export class ShoppingComponent {
+export class ShoppingComponent implements OnInit {
+  shoppingDetailCart: any = {};
 
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private customAlertService: CustomAlertService
+  ) { }
+
+  ngOnInit() {
+    this.getShoppingCart();
+  }
+
+  getShoppingCart() {
+    try {
+      this.shoppingCartService.getShoppingCart(`${apiURL}/usuario/carrito/ver`).subscribe({
+        next: (data: any) => {
+          this.shoppingDetailCart = data.shoppingDetailCart;
+          console.log(this.shoppingDetailCart);
+        },
+        error: (error: any) => {
+          this.customAlertService.sweetAlertPersonalizada('error', "Error", error.error.error);
+        }
+      })
+    } catch (error: any) {
+      console.log(error.error);
+    }
+  }
+
+  decreaseQuantity(item: any) {
+    if (item.Cantidad_Producto > 1) {
+      item.Cantidad_Producto--;
+      item.Subtotal_Venta = (item.Cantidad_Producto * item.Precio_Unitario).toFixed(2);
+    }
+  }
+
+  increaseQuantity(item: any) {
+    item.Cantidad_Producto++;
+    item.Subtotal_Venta = (item.Cantidad_Producto * item.Precio_Unitario).toFixed(2);
+  }
 }
