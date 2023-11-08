04d747657b2781420e726b1e101e0802edbc91f2
HectorGarcia01 <hg2779948@gmail.com>
2023-11-07 19:30:00 -0600

    Se implementó el servicio para la activación de cuenta

---
+++ src/app/auth/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivateAccountService } from '../../services/activate-account.service';
+import { CustomAlertService } from 'src/app/services/custom-alert.service';
+import { apiURL } from 'src/app/config/config';
+
+@Component({
+  selector: 'app-activate-account',
+  templateUrl: './activate-account.component.html',
+  styleUrls: ['./activate-account.component.css']
+})
+export class ActivateAccountComponent implements OnInit {
+  activateForm!: FormGroup;
+
+  constructor(
+    private activateAccountService: ActivateAccountService,
+    private customAlertService: CustomAlertService,
+    private router: Router
+  ) {
+    this.validateForm();
+  }
+
+  ngOnInit() {
+
+  }
+
+  /**
+   * Función privada para la definición de un formulario reactivo
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   */
+  private validateForm() {
+    this.activateForm = new FormGroup({
+      token: new FormControl('', [Validators.required])
+    });
+  }
+
+  /**
+   * Función para realizar una solicitud post para activar la cuenta de usuario
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función activateAccount del servicio de autenticación (activate-account.service)
+   */
+
+  onSubmit() {
+    try {
+      if (this.activateForm.valid) {
+        const token = this.activateForm.get('token')?.value;
+
+        this.activateAccountService.activateAccount(`${apiURL}/usuario/activar/cuenta`, token).subscribe({
+          next: (data: any) => {
+            this.customAlertService.sweetAlertPersonalizada('success', "Exitoso", data.msg);
+            this.router.navigate(['/login']);
+          },
+          error: (error: any) => {
+            console.log(error);
+            this.customAlertService.sweetAlertPersonalizada('error', "Error", error.error.error);
+          }
+        })
+      } else {
+        this.customAlertService.sweetAlertPersonalizada('error', "Campo vacío", "Para activar tu cuenta debes ingresar el token.");
+      }
+    } catch (error: any) {
+      console.log(error.error);
+    }
+  }
+}
