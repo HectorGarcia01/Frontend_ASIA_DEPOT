c185926391502e4af6751d7a34aaf0639b8772e4
HectorGarcia01 <hg2779948@gmail.com>
2023-11-10 06:16:04 -0600

    Modificación de la vista del detalle de producto, se agregaron validaciones a los botones para que se desactiven en caso de que no hayan productos

--- src/app/user/products/pages/product-detail/product-detail.component.ts
+++ src/app/user/products/pages/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/user/services/product.service';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { ShoppingCartService } from 'src/app/user/services/shopping-cart.service';
+import { ReviewsService } from 'src/app/user/services/reviews.service';
 import { CustomAlertService } from 'src/app/services/custom-alert.service';
 import { Product } from 'src/app/user/interfaces/product.interface';
 import { Category } from 'src/app/user/interfaces/category.interface';
@@ -17,11 +18,14 @@ export class ProductDetailComponent implements OnInit {
   product: Product = {} as Product;
   category: Category = {} as Category;
   increment: number = 1;
+  productReviews: any = [];
+  countReviews: number = 0;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private authService: AuthService,
+    private reviewsService: ReviewsService,
     private shoppingCartService: ShoppingCartService,
     private customAlertService: CustomAlertService
   ) { }
@@ -45,6 +49,7 @@ export class ProductDetailComponent implements OnInit {
 
       if (IdProduct) {
         this.getProductId(IdProduct);
+        this.getReviews(IdProduct);
       }
     });
   }
@@ -83,9 +88,36 @@ export class ProductDetailComponent implements OnInit {
       this.shoppingCartService.addProductCart(`${apiURL}/usuario/carrito/agregar`, body).subscribe({
         next: (data: any) => {
           this.customAlertService.sweetAlertPersonalizada('success', "Exitoso", data.msg);
+          this.getParamsId();
+          this.increment = 1;
         },
         error: (error: any) => {
-          this.customAlertService.sweetAlertPersonalizada('error', "Error", error.error.error);
+          this.customAlertService.sweetAlertPersonalizada('error', "Lo siento", `No hay sufiente stock de "${this.product.Nombre_Producto}"`);
+        }
+      });
+    } catch (error: any) {
+      console.log(error.error);
+    }
+  }
+
+  /**
+   * Función para consumir el servicio de ver reseñas
+   * Fecha creación: 06/10/2023
+   * Autor: Hector Armando García González
+   * Referencias: 
+   *            Función getCustomerProfile del servicio de usuarios (user-services.service),
+   *            Función sweetAlertPersonalizada del servicio de alerta personalizada (custom-alert.service)  
+   */
+
+  getReviews(id: any) {
+    try {
+      this.reviewsService.getReviews(`${apiURL}/usuario/ver/todas/valoraciones/producto`, id).subscribe({
+        next: (data: any) => {
+          this.productReviews = data.productReviews;
+          this.countReviews = this.productReviews.length;
+        },
+        error: (error: any) => {
+          console.log(error.error.error);
         }
       });
     } catch (error: any) {
