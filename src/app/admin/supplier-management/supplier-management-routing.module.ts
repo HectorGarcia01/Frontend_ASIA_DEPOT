18ddee753be4b53c0056d796677de9b5765d015a
HectorGarcia01 <hg2779948@gmail.com>
2023-11-11 19:21:51 -0600

    Creación y configuración de rutas para los componentes de la gestión de proveedores

--- src/app/admin/supplier-management/supplier-management-routing.module.ts
+++ src/app/admin/supplier-management/supplier-management-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewSupplierComponent } from './pages/new-supplier/new-supplier.component';
+import { ListSuppliersComponent } from './pages/list-suppliers/list-suppliers.component';
+import { ListSupplierComponent } from './pages/list-supplier/list-supplier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'create/supplier', pathMatch: 'full' },
+
+  {
+    path: 'create/supplier',
+    component: NewSupplierComponent,
+  },
+  {
+    path: 'list/suppliers',
+    component: ListSuppliersComponent,
+  },
+  {
+    path: 'view/supplier/:id',
+    component: ListSupplierComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
