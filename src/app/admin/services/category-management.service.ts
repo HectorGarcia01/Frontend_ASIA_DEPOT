ae8baf01f0fc1a144b9e8449d944833864a64a53
HectorGarcia01 <hg2779948@gmail.com>
2023-11-12 18:23:03 -0600

    Creación de servicios para solicitudes http hacia el backend para la gestión de categorías

---
+++ src/app/admin/services/category-management.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryManagementService {
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) { }
+
+  /**
+   * Función para realizar una solicitud post para crear una categoría
+   * Fecha creación: 20/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función getCookieAuth del servicio de autenticación (auth.service)
+   */
+
+  createCategory(url: string, category: any): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.post<any>(url, category, { headers });
+  }
+
+  /**
+     * Función para realizar una solicitud get para ver todas las categorías
+     * Fecha creación: 20/10/2023
+     * Autor: Hector Armando García González
+     * Referencias:
+     *              Función getCookieAuth del servicio de autenticación (auth.service)
+     */
+
+  getCategories(url: string): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  /**
+   * Función para realizar una solicitud get para ver una categoría por id
+   * Fecha creación: 20/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función getCookieAuth del servicio de autenticación (auth.service)
+   */
+
+  getCategoryId(url: string, id: number): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    url = `${url}/${id}`;
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  /**
+   * Función para realizar una solicitud patch para actualizar una categoría por id
+   * Fecha creación: 20/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función getCookieAuth del servicio de autenticación (auth.service)
+   */
+
+  updateCategory(url: string, id: string, category: any): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    url = `${url}/${id}`;
+
+    return this.http.patch<any>(url, category, { headers });
+  }
+
+  /**
+   * Función para realizar una solicitud delete para eliminar una categoría por id
+   * Fecha creación: 20/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función getCookieAuth del servicio de autenticación (auth.service)
+   */
+
+  deleteCategory(url: string, id: number): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    url = `${url}/${id}`;
+
+    return this.http.delete<any>(url, { headers });
+  }
+
+  /**
+   * Función para realizar una solicitud patch para activar una categoría por id
+   * Fecha creación: 20/10/2023
+   * Autor: Hector Armando García González
+   * Referencias:
+   *              Función getCookieAuth del servicio de autenticación (auth.service)
+   */
+
+  activateCategory(url: string, id: number): Observable<any> {
+    const token = this.authService.getCookieAuth();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    url = `${url}/${id}`;
+
+    return this.http.patch<any>(url, null, { headers });
+  }
+}
