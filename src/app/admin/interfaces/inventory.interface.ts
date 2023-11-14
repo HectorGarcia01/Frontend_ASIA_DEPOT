2237c142b0245ad36718e34b576db45e178b6d37
HectorGarcia01 <hg2779948@gmail.com>
2023-11-14 14:02:14 -0600

    Creación de la interface para ver el detalle de inventario

--- src/app/admin/interfaces/inventory.interface.ts
+++ src/app/admin/interfaces/inventory.interface.ts
@@ -10,3 +10,22 @@ export interface getInventory {
     };
     createdAt: string;
 }
+
+export interface getInventoryDetail {
+    id: number;
+    Tipo_Movimiento: string;
+    Cantidad_Movimiento: string;
+    Monto_Movimiento: string;
+    empleado: {
+        id: number;
+        Nombre_Empleado: string;
+        Apellido_Empleado: string;
+    };
+    producto: {
+        id: number;
+        Nombre_Producto: string;
+        Precio_Venta: number;
+        Precio_Compra?: number;
+    };
+    createdAt: string;
+}
